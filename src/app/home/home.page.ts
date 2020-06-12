fb18bdb8561667f40759b5c576f4a9f39da3ae99
easyadin <tobiayokunnu@gmail.com>
2020-06-12 22:02:28 +0400

    favicon and header border

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private scrollObserver: IntersectionObserver;
+  isScrolling = false;
+  constructor(
+    animationCtrl: AnimationController,
+  ) { }
+
+  ngAfterViewInit() {
+    //scroll animation with intersection observer
+    this.scrollObserver = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting === true) {
+        // page is scrolling
+        this.isScrolling = true;
+        // apply border bottom to header
+
+        // apply reveal animation to sections
+      }
+      else {
+        // page is not scrolling
+        this.isScrolling = false;
+      }
+    }, {
+      threshold: .3
+    });
+    //get all sections
+    const sections = document.querySelectorAll('section');
+    // loop over all section and ignore the first section
+    for (let i = 1; i < sections.length; i++) {
+      this.scrollObserver.observe(sections[i])
+    }
+  }
+
 
 }
