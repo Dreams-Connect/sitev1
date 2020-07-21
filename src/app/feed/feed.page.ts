db716eefb41a4d516377aa217eb207a337adea6d
easyadin <tobiayokunnu@gmail.com>
2020-07-21 07:04:38 +0400

    button color update

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import videojs from 'video.js';
+
 
 @Component({
   selector: 'app-feed',
@@ -9,8 +11,26 @@ export class FeedPage implements OnInit {
   constructor() { }
 
   scrollYPosition = 0;
-
+  scrollObserver: IntersectionObserver
   ngOnInit() {
+    /// auto play video on interception
+    this.scrollObserver = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting){
+        console.log(entries[0])
+      }
+      else {
+        console.log(entries[0])
+      }
+
+    }, {
+      threshold: 1
+    });
+
+    // get players
+    const players = document.querySelectorAll('video');
+    for (let i = 0; i < players.length; i++) {
+      this.scrollObserver.observe(players[i])
+    }
   }
 
 
@@ -20,9 +40,7 @@ export class FeedPage implements OnInit {
 
   logScrolling(event) {
     this.scrollYPosition = event.detail.scrollTop;
-    console.log(this.scrollYPosition)
   }
-  logScrollEnd() {
 
-  }
+  logScrollEnd() { }
 }
